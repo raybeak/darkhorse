2cd52f4b7e91f989e0424ca3303dfe651a92f89d
jinshim-commits <tlathwls0518@gmail.com>
2025-12-16 16:09:05 +0900

    setup.py 주석 제거 및 launch 파일 설치 설정 추가
    1)ROS만 먼저 source
    source /opt/ros/humble/setup.bash
    
    2) 워크스페이스를 통째로 클린
    cd ~/darkhorse
    rm -rf build install log
    
    3) 다시 빌드
    colcon build --symlink-install
    
    4) 다시 source
    source ~/darkhorse/install/setup.bash
    
    5) 확인
    ros2 pkg prefix my_bringup
    
    6)실행결과
    /home/jinshim/darkhorse/install/my_bringup
    
    7)런치파일 실행
    ros2 launch my_bringup system_no_bt.launch.py

--- src/my_bringup/setup.py
+++ src/my_bringup/setup.py
@@ -1,33 +1,3 @@
-# from setuptools import setup
-# import os
-# from glob import glob  # <--- 1. 추가!
-
-# package_name = 'my_bringup'
-
-# setup(
-#     name=package_name,
-#     version='0.0.0',
-#     packages=[package_name],
-#     data_files=[
-#         ('share/ament_index/resource_index/packages',
-#             ['resource/' + package_name]),
-#         ('share/' + package_name, ['package.xml']),
-#         # ---------------------------------------------------
-#         # 2. 아래 줄을 복사해서 넣으세요!
-#         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),        # ---------------------------------------------------
-#     ],
-#     install_requires=['setuptools'],
-#     zip_safe=True,
-#     maintainer='wego',
-#     maintainer_email='wego@todo.todo',
-#     description='System Bringup Package',
-#     license='TODO: License declaration',
-#     tests_require=['pytest'],
-#     entry_points={
-#         'console_scripts': [],
-#     },
-# )
-
 from setuptools import setup
 import os
 from glob import glob
@@ -42,7 +12,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # launch 파일 설치
         (os.path.join('share', package_name, 'launch'),
          glob(os.path.join('launch', '*.py'))),
     ],
@@ -53,9 +22,5 @@ setup(
     description='System Bringup Package',
     license='TODO: License declaration',
     tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            # 여긴 비워둬도 됨 (launch는 console_scripts 없어도 됨)
-        ],
-    },
+    entry_points={'console_scripts': []},
 )
